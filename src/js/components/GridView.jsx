0f1fe8817dc5937197ce40b45c931c8f1e36bfb3
kellyfreet13 <kelly.freet13@yahoo.com>
2019-05-20 10:10:14 -0700

    added dynamic gpa calculation

--- src/js/components/GridView.jsx
+++ src/js/components/GridView.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
 import { orderBy, filterBy } from '@progress/kendo-data-query';
+import { has } from 'lodash';
 
 import transcript from '../transcript.json';
 
@@ -28,13 +29,58 @@ class GridView extends Component {
         });
     }
 
+    calculateGpa = () => {
+        let totalFiltered = filterBy(this.state.gridData, this.state.filter);
+        let len = totalFiltered.length;
+        let weightedGpaSum = 0;
+        let weightedUnitsSum = 0;  // total possible weighted units
+        for (let i = 0; i < len; i++){
+            let cur = totalFiltered[i];
+            let units = cur['Units'];
+            switch (cur['GradeReceived']) {
+                case "A":
+                    weightedGpaSum += units*4;
+                    break;
+                case "B":
+                    weightedGpaSum += units*3;
+                    break;
+                case "C":
+                    weightedGpaSum += units*2;
+                    break;
+            }
+            weightedUnitsSum += units*4;
+        }
+        let gpa = (weightedGpaSum / weightedUnitsSum) * 4;
+        return gpa;
+    }
+
+    getGpaColor = (gpa) => {
+        if (gpa > 3.5) {
+            return { backgroundColor: '#00bf23' };
+        } else if (gpa > 3.0 && gpa < 3.5){
+            return { backgroundColor: '#dfe200' };
+        } else if (gpa > 2.5 && gpa < 3.0) {
+            return { backgroundColor: '#e29e00'};
+        }
+    }
+
+    // TODO: add predefined filter buttons with "Current GPA w/filters" and perhaps some graphical display
     render() {
         let orderedData = orderBy(this.state.gridData, this.state.sort);
         let pagedData = orderedData.slice(this.state.skip, this.state.take + this.state.skip);
+        let filteredData = filterBy(pagedData, this.state.filter);
+        let gpa = this.calculateGpa().toPrecision(4);
+        let gpaColor = this.getGpaColor(gpa);
         return (
             <div id="grid-container">
                 <div id="grid-descriptor">
-                    Initially filtered by "Upper" division Courses. This makes my GPA nicer :)
+                    Initially filtered by "Upper" division Courses. This is what matters (i.e. better GPA)
+                </div>
+                <div id="gpa-descriptor">
+                    Current GPA with filters:
+                    <span id="circle-inner" style={gpaColor}>
+                        <span id="gpa">{gpa}</span>
+                    </span>
                 </div>
 
                 <Grid
@@ -50,13 +96,13 @@ class GridView extends Component {
                     onFilterChange={(e) => {
                         this.setState({ filter: e.filter });
                     }}
-                    data={filterBy(pagedData, this.state.filter)}
+                    data={filteredData}
                 >
                     <Column field="CourseName" title="Name" width="120px" />
                     <Column field="CourseBriefDescription" title="Description" />
                     <Column field="Category" title="Category" width="120px" />
                     <Column field="Semester" title="Semester" width="120px" />
-                    <Column field="Year" title="Year" width="120px" />
+                    <Column field="Year" title="Year" width="140px" />
                     <Column field="GradeReceived" title="Grade" />
                     <Column field="Division" title="Division" />
                 </Grid>
